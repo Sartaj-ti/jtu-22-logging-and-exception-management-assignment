1c19ff6e0487f3d9c0b163e6babc997a3db3ff80
Sartaj-ti <sartaj.islam@trilogy.com>
2022-12-15 16:42:06 +0000

    Services logs and exceptions

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -14,6 +14,11 @@ what exceptions can be thrown here?
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
     adf_obj = json.loads(adf_json, object_hook=lambda d: SimpleNamespace(**d))
+    
     if not adf_obj:
         raise TypeError(f'{adf_json} cannot be converted to object')
+    for key in adf_json.keys():
+        if not adf_obj[key]:
+            raise KeyError(f'{adf_obj} doesn\'t contains key={key}')
+    
     return adf_obj
--- fast_api_als/services/verify_phone_and_email.py
+++ fast_api_als/services/verify_phone_and_email.py
@@ -2,6 +2,8 @@ import time
 import httpx
 import asyncio
 import logging
+import fastapi
+
 from fast_api_als.constants import (
     ALS_DATA_TOOL_EMAIL_VERIFY_METHOD,
     ALS_DATA_TOOL_PHONE_VERIFY_METHOD,
@@ -12,17 +14,21 @@ from fast_api_als.constants import (
 How can you write log to understand what's happening in the code?
 You also trying to undderstand the execution time factor.
 """
+logger = logging.getLogger(__name__)
 
 async def call_validation_service(url: str, topic: str, value: str, data: dict) -> None:  # 2
     if value == '':
         return
+
     async with httpx.AsyncClient() as client:  # 3
         response = await client.get(url)
+    if response.status != 200:
+        raise fastapi.HTTPException(status_code=response.status, detail=f'GET request failed on {url}')
 
     r = response.json()
+
     data[topic] = r
-    logger.info(f'{topic} validation data: {data}')
-    
+
 
 async def verify_phone_and_email(email: str, phone_number: str) -> bool:
     email_validation_url = '{}?Method={}&RequestKey={}&EmailAddress={}&OutputFormat=json'.format(
@@ -43,10 +49,14 @@ async def verify_phone_and_email(email: str, phone_number: str) -> bool:
         call_validation_service(email_validation_url, "email", email, data),
         call_validation_service(phone_validation_url, "phone", phone_number, data),
     )
+    logger.info(f'data fetched for email and phone verification = {data}')
+
     if "email" in data:
         if data["email"]["DtResponse"]["Result"][0]["StatusCode"] in ("0", "1"):
             email_valid = True
     if "phone" in data:
         if data["phone"]["DtResponse"]["Result"][0]["IsValid"] == "True":
             phone_valid = True
+    logger.info(f'phone and email verification with email={email}, phone={phone_number}: email_valid={email_valid}, phone_valid={phone_valid}')
+    
     return email_valid | phone_valid
